2b848a2b63305edb7823b37ea92c0cef57de4f59
cwilkens <scavengerq@gmail.com>
2021-02-25 22:29:16 -0600

    initial project creation
    Project creation from Visual Studio - ASP.NET Core web application, .NET Core 5.0.

---
+++ DndSpells/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DndSpells.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
